af25c625eef761abe8cafaa145fd5ceb7f32411a
Derek Tran <derektran.dt@gmail.com>
2023-07-02 08:35:02 +1000

    Refactored activities in q1

--- frontend/pages/Question1.jsx
+++ frontend/pages/Question1.jsx
@@ -14,20 +14,42 @@ import Hiking from "../assets/hiking.png";
 import Partying from "../assets/partying.png";
 import ProgressBar from "../components/ProgressBar1.png";
 import PrimaryButton from "../ui-kit/primary-btn";
+import ListActivities from "../components/ListActivities";
 
 const Question1 = ({ navigation }) => {
   const [selected, setSelected] = React.useState("");
 
-  const handlePress = (index) => {
-    if (selected === index) {
+  const handlePress = (Activity) => {
+    if (selected === Activity) {
       setSelected(null);
     } else {
-      setSelected(index);
+      setSelected(Activity);
     }
   };
-  const isBoxSelected = (index) => {
-    return selected === index ? styles.selectedBorder : styles.defaultBorder;
-  };
+
+  activities = [
+    {
+      activity: "Walking",
+      image: Walking,
+      tintColor: "#DDB6B6",
+    },
+    {
+      activity: "Driving",
+      image: Driving,
+      tintColor: "#AAD8E7",
+    },
+    {
+      activity: "Hiking",
+      image: Hiking,
+      tintColor: "#EAAC89",
+    },
+    {
+      activity: "Partying",
+      image: Partying,
+      tintColor: "#A59BE7",
+    },
+  ];
+
   return (
     <View style={styles.pageContainer}>
       <View style={styles.barContainer}>
@@ -35,52 +57,11 @@ const Question1 = ({ navigation }) => {
       </View>
       <Text style={styles.title}>What are you up to?</Text>
       <View style={styles.container}>
-        <View style={styles.selectBox}>
-          <View style={styles.activityContainer}>
-            <TouchableOpacity
-              style={styles.pressableContainer}
-              onPress={() => handlePress('Walking')}
-            >
-              <View style={[styles.imageContainer, isBoxSelected('Walking')]}>
-                <Image source={Walking} style={styles.activity1} />
-                <Text>Walking</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.activityContainer}>
-            <TouchableOpacity
-              style={styles.pressableContainer}
-              onPress={() => handlePress('Driving')}
-            >
-              <View style={[styles.imageContainer, isBoxSelected('Driving')]}>
-                <Image source={Driving} style={styles.activity2} />
-                <Text>Driving</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.activityContainer}>
-            <TouchableOpacity
-              style={styles.pressableContainer}
-              onPress={() => handlePress('Hiking')}
-            >
-              <View style={[styles.imageContainer, isBoxSelected('Hiking')]}>
-                <Image source={Hiking} style={styles.activity3} />
-                <Text>Hiking</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.activityContainer}>
-            <TouchableOpacity
-              style={styles.pressableContainer}
-              onPress={() => handlePress('Partying')}
-            >
-              <View style={[styles.imageContainer, isBoxSelected('Partying')]}>
-                <Image source={Partying} style={styles.activity4} />
-                <Text>Partying</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <ListActivities
+          activities={activities}
+          selectedActivity={selected}
+          handlePress={handlePress}
+        />
         <PrimaryButton
           title="Continue"
           onPress={() => navigation.navigate("Question2")}
@@ -102,64 +83,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     gap: 40,
     paddingHorizontal: 20,
-  },
-  selectBox: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap",
-    flexDirection: "row",
-    width: "100%",
-    height: "60%",
-    gap: 20,
-  },
-  activityContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "45%",
-    aspectRatio: "1 / 1",
-  },
-  pressableContainer: {
-    width: "100%",
-    height: "100%",
-  },
-  imageContainer: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 10,
-    borderRadius: 10,
-  },
-  activity1: {
-    width: "60%",
-    height: "60%",
-    tintColor: "#DDB6B6",
-  },
-  activity2: {
-    width: "60%",
-    height: "60%",
-    tintColor: "#AAD8E7",
-  },
-  activity3: {
-    width: "60%",
-    height: "60%",
-    tintColor: "#EAAC89",
-  },
-  activity4: {
-    width: "60%",
-    height: "60%",
-    tintColor: "#A59BE7",
-  },
-  defaultBorder: {
-    borderWidth: 2,
-    borderColor: "#e5e5e5",
-  },
-  selectedBorder: {
-    borderWidth: 2,
-    borderColor: "#e5e5e5",
-    backgroundColor: "#e5e5e5",
   },
   progress: {
     width: 330,
